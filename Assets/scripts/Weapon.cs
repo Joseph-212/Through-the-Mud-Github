bda2c31c2846cd81926d3389912dfe650e6537b9
Joseph-212 <josepg.ortizjimenez18@gmail.com>
2026-08-18 22:28:53 -0600

    I kinda achieved a basic weapon system
    it is buggy

---
+++ Assets/scripts/Weapon.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    public float bulletVelocity = 30f;
+    public float bulletPrefabLifeTime = 3f;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            FireWeapon();
+        }
+        
+    }
+
+    public void FireWeapon()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, Quaternion.identity);
+
+        bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.forward.normalized * bulletVelocity, ForceMode.Impulse);
+
+        StartCoroutine(DestroyBulletAfterTime(bullet, bulletPrefabLifeTime));
+    }
+
+    private IEnumerator DestroyBulletAfterTime(GameObject bullet, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(bullet);
+    }
+}
